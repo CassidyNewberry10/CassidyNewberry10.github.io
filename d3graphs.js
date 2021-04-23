e2cc798f70af09179bc34cf880d8335994bb8ca4
CassidyNewberry10 <82054894+CassidyNewberry10@users.noreply.github.com>
2021-04-23 15:05:31 -0700

    Update d3graphs.js

--- d3graphs.js
+++ d3graphs.js
@@ -34,8 +34,7 @@ const render= data=>{
     .enter().append('rect')
     .attr('y', d=> yScale(yValue(d)))
     .attr('width', d=> xScale(xValue(d)))
-    .attr('height', yScale.bandwidth())
-    .attr('fill', 'red');
+    .attr('height', yScale.bandwidth());
 
     svg.append("text")
     .attr("class", "x label")
