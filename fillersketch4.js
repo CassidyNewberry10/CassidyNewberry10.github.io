cd2192134e408d08114e1fe06472ab4542b60064
CassidyNewberry10 <82054894+CassidyNewberry10@users.noreply.github.com>
2021-05-18 15:52:22 -0700

    Add files via upload

---
+++ fillersketch4.js
@@ -0,0 +1,83 @@
+//this is a filller sketch and it summarizes the fourth objective
+//This is also where the data sources are listed
+
+
+ var margin = {top:100 , bottom: 100 , left: 20, right:250},
+        width =1200 - margin.left - margin.right,
+        height = 250 - margin.top - margin.bottom;
+
+        //scales
+
+    var svg =d3.select("body").append("svg")
+                .attr("width", width +margin.left +margin.right)
+                .attr("height" , height +margin.top +margin.bottom)
+                .append("g")
+                .attr("transform", "translate (" +margin.left +","+ margin.top + ")")
+
+
+    svg.append("text")
+        .style("font-size" , 20)
+        .attr("transform", "translate(100,0)")
+        .attr("x", -50)
+        .attr("y", -80)
+        .attr("fill", "black")
+        .text("Objective 4 Summary: This graph shows that women experienced higher rates of unemployment during the peak of the pandemic."); 
+
+    svg.append("text")
+        .style("font-size" , 20)
+        .attr("transform", "translate(100,0)")
+        .attr("x", -50)
+        .attr("y", -60)
+        .attr("fill", "black")
+        .text("The unemployment rates are nearly the same now, but COVID caused a gap to exist in many months of 2020.");
+
+
+    svg.append("text")
+        .style("font-size" , 30)
+        .attr("transform", "translate(100,0)")
+        .attr("x", -50)
+        .attr("y", -20)
+        .attr("fill", "purple")
+        .text("Project Sources:");
+
+    svg.append("text")
+        .style("font-size" , 20)
+        .attr("transform", "translate(100,0)")
+        .attr("x", -50)
+        .attr("y", 0)
+        .attr("fill", "purple")
+        .text("objective 1: https://www.kaggle.com/nilimajauhari/glassdoor-analyze-gender-pay-gap/activity");
+
+    svg.append("text")
+        .style("font-size" , 20)
+        .attr("transform", "translate(100,0)")
+        .attr("x", -50)
+        .attr("y", 20)
+        .attr("fill", "purple")
+        .text("objective 2: https://www.kaggle.com/nilimajauhari/glassdoor-analyze-gender-pay-gap/activity");
+
+    svg.append("text")
+        .style("font-size" , 20)
+        .attr("transform", "translate(100,0)")
+        .attr("x", -50)
+        .attr("y", 40)
+        .attr("fill", "purple")
+        .text("objective 3: https://www.bls.gov/news.release/pdf/wkyeng.pdf");
+
+    svg.append("text")
+        .style("font-size" , 20)
+        .attr("transform", "translate(100,0)")
+        .attr("x", -50)
+        .attr("y", 60)
+        .attr("fill", "purple")
+        .text("objective 4:  https://beta.bls.gov/dataViewer/view/timeseries/LNS14000025");
+
+    svg.append("text")
+        .style("font-size" , 20)
+        .attr("transform", "translate(100,0)")
+        .attr("x", 48)
+        .attr("y", 80)
+        .attr("fill", "purple")
+        .text("https://beta.bls.gov/dataViewer/view/timeseries/LNS14000026");
+
+    
---
+++ sketch4final.js
@@ -0,0 +1,179 @@
+//GRAPH 4: Base Pay by Senority, Gender
+//this is the first jitter plot 
+//This uses the file SenorityBasePayGenderJob.csv
+
+
+    var margin = {top:100 , bottom: 100 , left: 20, right:250},
+        width =1200 - margin.left - margin.right,
+        height = 1000 - margin.top - margin.bottom;
+
+        //scales
+
+    var svg =d3.select("body").append("svg")
+                .attr("width", width +margin.left +margin.right)
+                .attr("height" , height +margin.top +margin.bottom)
+                .append("g")
+                .attr("transform", "translate (" +margin.left +","+ margin.top + ")");
+
+const titleText = 'Base Salary by Seniority and Gender';
+
+const render = data => {
+    const xValue = d => d.Seniority;
+    const yValue = d => d.BasePay;
+    const Gender= d => d.Gender;
+    const JobType= d => d.Category;
+    const margin = {top: 20, right: 20, bottom: 20, left: 100};
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+
+
+    const xScale = d3.scaleLinear()
+        .domain([.3, 5.4])
+        .range([0, width]);
+
+    const yScale = d3.scaleLinear()
+                .range([height, 0]);
+
+   	yScale.domain([-9500, 180000]);
+
+
+    const genderVals = d3.map(data, function(d){return d.Gender;}).keys();
+
+    const g = svg.append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    
+    const yAxis = d3.axisLeft(yScale);
+
+    g.append('g')
+        .call(yAxis)
+        .attr("font-size", 18);
+
+   
+    const xAxis = d3.axisBottom(xScale).ticks(5);
+
+    g.append('g').call(xAxis)
+        .attr('transform', `translate(0,${innerHeight})`)
+        .attr("font-size", 20);
+
+    g.append('text')
+        .attr('class', 'title')
+        .attr("font-size", 20)
+        .attr('y', -50)
+        .attr('x', 350)
+        .text(titleText);
+
+    g.append('text')
+        .attr('class', 'title')
+        .attr("font-size", 18)
+        .attr('y', 50)
+        .attr('x', 150)
+        .text("**Hover over to see each circles value");
+
+    g.append('text')
+        .attr('class', 'title')
+        .attr("font-size", 20)
+        .attr('y', 800)
+        .attr('x', 450)
+        .text('Seniority');
+
+    g.append("text")
+        .attr("class", "y label")
+        .attr("text-anchor", "end")
+        .attr("y", -100)
+        .attr("x", -300)
+        .attr("dy", ".75em")
+        .attr("transform", "rotate(-90)")
+        .text("Base Salary ($)")
+        .attr("font-size", 20);
+
+    const div = d3
+        .select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    
+
+    g.selectAll(".dot").data(data)
+      .enter().append('circle')
+      .attr('cy', d=> yScale(yValue(d)))
+      .attr('cx', d=> xScale(xValue(d) + Math.random()*.5))
+      .attr('r', d=> 5)
+      .style("fill", function(d) {
+            if (d.Gender == "Female") {return "purple"}
+            else { return "blue" }
+        ;})
+      .on("mouseover", (event, d) => {
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div.html(`Gender : ${d.Gender} <br/> Base Salary : ${d["BasePay"]}`)
+                .style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 20) + "px");
+        })
+        .on("mouseout", _ => {
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
+        })
+        .on("mousemove", event => {
+            div.style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 20) + "px")
+        })
+
+
+     g.append('circle')
+    .attr('cx',990)
+    .attr('cy', 250)
+    .attr('r', 20)
+    .style('fill', 'blue');
+
+    g.append('circle')
+    .attr('cx',990)
+    .attr('cy', 300)
+    .attr('r', 20)
+    .style('fill', 'purple');
+
+    g.append('text')
+        .attr('class', 'title')
+        .attr("font-size", 20)
+        .attr('y', 260)
+        .attr('x', 1010)
+        .text('Male');
+
+    g.append('text')
+        .attr('class', 'title')
+        .attr("font-size", 20)
+        .attr('y', 310)
+        .attr('x', 1010)
+        .text('Female');
+
+    g.append('text')
+        .attr('class', 'title')
+        .attr("font-size", 20)
+        .attr('y', 200)
+        .attr('x', 970)
+        .text('Color Scale');
+
+
+    svg.append("text")
+        .style("font-size" ,15)
+        .attr("transform", "translate(100,0)")
+        .attr("x", -50)
+        .attr("y", 840)
+        .text("**This chart used a random 50% of the dataset. Here we can see that all of the highest values are blue, and many of the lower values are purple. The top 10 base salaries are all blue dots.");
+ 
+
+};
+
+d3.csv('SenorityBasePayGenderJob.csv').then(data => { 
+    data.forEach(d =>{
+        d.Seniority= +d.Seniority;
+        d.BasePay= +d.BasePay;
+        d.Gender= d.Gender;
+        d.JobType= d.Category;
+    })
+   render(data);
+
+});
